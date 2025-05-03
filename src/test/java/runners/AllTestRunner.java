9dbaf3bff95d2b632ea74ca01cf6f5f559420a4f
AzimKorkmaz <azimkorkmaz@gmail.com>
2025-05-04 02:05:03 +0300

    Initial commit

---
+++ src/test/java/runners/AllTestRunner.java
@@ -0,0 +1,29 @@
+package runners;
+
+import com.aventstack.extentreports.service.ExtentService;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.*;
+import org.testng.annotations.*;
+
+import java.time.LocalDateTime;
+
+@CucumberOptions(features = {"src/test/java/featureFiles"},
+        glue = {"stepDefinitions"},
+        plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+public class AllTestRunner extends AbstractTestNGCucumberTests {
+
+    @AfterClass
+    public void writeExtendReport() {
+        ExtentService.getInstance().setSystemInfo("--------------------", "--------------------");
+        ExtentService.getInstance().setSystemInfo("Windows User Name", System.getProperty("user.name"));
+        ExtentService.getInstance().setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+        ExtentService.getInstance().setSystemInfo("Browser", GWD.threadBrowserName.get());
+        ExtentService.getInstance().setSystemInfo("Execution Date", LocalDateTime.now().toString());
+        ExtentService.getInstance().setSystemInfo("User Name", "Bug Fathers");
+        ExtentService.getInstance().setSystemInfo("Team Name", "Team#4");
+        ExtentService.getInstance().setSystemInfo("Application Name", "E-Junkie Demo Shop");
+        ExtentService.getInstance().setSystemInfo("Test Tag", "All Test Runner");
+        ExtentService.getInstance().setSystemInfo("Operating System Info", System.getProperty("os.name"));
+        ExtentService.getInstance().setSystemInfo("Department", "QA");
+    }
+}
---
+++ src/test/java/utilities/ReusableMethods.java
@@ -0,0 +1,32 @@
+package utilities;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class ReusableMethods {
+    public Actions action;
+
+    public ReusableMethods() {
+        action = new Actions(GWD.getDriver());
+    }
+
+    public void mySendKeys(WebElement element, String text) {
+        GWD.getWait().until(ExpectedConditions.visibilityOf(element));
+        scrollToElement(element);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public void myClick(WebElement element) {
+        GWD.getWait().until(ExpectedConditions.elementToBeClickable(element));
+        scrollToElement(element);
+        element.click();
+    }
+
+    public void scrollToElement(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
+        js.executeScript("arguments[0].scrollIntoView();", element);
+    }
+}
