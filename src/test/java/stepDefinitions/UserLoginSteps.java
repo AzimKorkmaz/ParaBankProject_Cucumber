1b86f48b1bbfb414cc550af16eb82abb7c63d4e5
NuriOzturk <nroztrk17@gmail.com>
2025-05-05 20:11:51 +0300

    US_602 updated.

--- src/test/java/stepDefinitions/UserLoginSteps.java
+++ src/test/java/stepDefinitions/UserLoginSteps.java
@@ -3,12 +3,19 @@ package stepDefinitions;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import pages.LeftNav;
 import pages.RegisterAndLogin;
 import utilities.ConfigReader;
 import utilities.GWD;
 
 public class UserLoginSteps {
     RegisterAndLogin ral = new RegisterAndLogin();
+    LeftNav ln = new LeftNav();
+
+    @When("The user clicks on the logout button")
+    public void theUserClicksOnTheLogoutButton() {
+        ln.myClick(ln.logOutButton);
+    }
 
     @When("The user fills in the login information and clicks the login button")
     public void theUserFillsInTheLoginInformationAndClicksTheLoginButton() {
@@ -22,5 +29,19 @@ public class UserLoginSteps {
     public void theUserShouldBeRedirectedToTheAccountOverviewPage() {
         GWD.getWait().until(ExpectedConditions.visibilityOf(ral.accountOverview));
         Assert.assertTrue(ral.accountOverview.isDisplayed());
+    }
+
+    @When("The user attempts to log in with incorrect login credentials")
+    public void theUserAttemptsToLogInWithIncorrectLoginCredentials() {
+        GWD.getWait().until(ExpectedConditions.visibilityOf(ral.loginUsername));
+        ral.mySendKeys(ral.loginUsername, ConfigReader.getProperty("wrongUsername"));
+        ral.mySendKeys(ral.loginPassword, ConfigReader.getProperty("wrongPassword"));
+        ral.myClick(ral.loginButton);
+    }
+
+    @Then("An error message should be displayed")
+    public void anErrorMessageShouldBeDisplayed() {
+        GWD.getWait().until(ExpectedConditions.visibilityOf(ral.errorMessage));
+        Assert.assertTrue(ral.errorMessage.isDisplayed());
     }
 }
