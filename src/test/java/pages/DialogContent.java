58fed35ae8b3b0c9f368db2817c3a481fb25b175
rftbtr <rifatbatir@gmail.com>
2025-05-05 11:18:07 +0300

    "DialogContent.java" Update

--- src/test/java/pages/DialogContent.java
+++ src/test/java/pages/DialogContent.java
@@ -1,6 +1,8 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.GWD;
 import utilities.ReusableMethods;
@@ -12,4 +14,37 @@ public class DialogContent extends ReusableMethods {
         PageFactory.initElements(GWD.getDriver(), this);
         this.action = new Actions(GWD.getDriver());
     }
+/// Bill Payment Service LOCATORS
+
+    @FindBy(name = "payee.name")
+    public WebElement payeeNameField;
+
+    @FindBy(name = "payee.address.street")
+    public WebElement payeeAddressStreetField;
+
+    @FindBy(name = "payee.address.city")
+    public WebElement payeeAddressCityField;
+
+    @FindBy(name = "payee.address.state")
+    public WebElement payeeAddressStateField;
+
+    @FindBy(name = "payee.address.zipCode")
+    public WebElement payeeAddressZipCodeieldField;
+
+    @FindBy(name = "payee.phoneNumber")
+    public WebElement payeePhoneNumberField;
+
+    @FindBy(name = "payee.accountNumber")
+    public WebElement payeeAccountNumberField;
+
+    @FindBy(name = "amount")
+    public WebElement amountField;
+
+    @FindBy(name = "fromAccountId")
+    public WebElement fromAccountIdField;
+
+    @FindBy(xpath = "//input[@value='Send Payment']")
+    public WebElement SendPaymentButton;
+
+
 }
